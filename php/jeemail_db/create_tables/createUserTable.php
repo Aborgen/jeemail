201cce11ea9f8866323715077778a31572a764ee
Alexander Borgen <AlexanderBorgen@gmail.com>
2017-10-24 17:19:02 -0700

    Add class that enables adding to User table in database

--- php/jeemail_db/create_tables/createUserTable.php
+++ php/jeemail_db/create_tables/createUserTable.php
@@ -6,10 +6,10 @@
     $table = 'User';
     $sql = "CREATE TABLE IF NOT EXISTS {$table}(
         UserID INT(11) AUTO_INCREMENT PRIMARY KEY,
-        first_name VARCHAR(255) NOT NULL,
-        last_name VARCHAR(255) NOT NULL,
-        username VARCHAR(255) NOT NULL,
-        email VARCHAR(255) NOT NULL,
+        first_name VARCHAR(64) NOT NULL,
+        last_name VARCHAR(64) NOT NULL,
+        username VARCHAR(64) NOT NULL,
+        email VARCHAR(64) NOT NULL,
         pass VARCHAR(255) NOT NULL,
         User_ImagesID INT(11) NOT NULL
         );";
