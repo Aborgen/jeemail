bea562a31d2885c616e9db49a23215afdf5e79a7
Alexander Borgen <AlexanderBorgen@gmail.com>
2018-04-23 17:30:37 -0700

    Add GET parameter to /details route for dev purposes

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 use App\Service\MemberInterface;
 class MemberController extends Controller
@@ -11,9 +12,13 @@ class MemberController extends Controller
     /**
      * @Route("/details", name="details")
      */
-    public function index(MemberInterface $interface): object
+    public function index(MemberInterface $interface, Request $request): object
     {
-        $id = 23;
+        $id = $request->get('id');
+        if(!isset($id)){
+            $id = -100;
+        }
+
         $interface->setId($id);
         $member = $interface->hydrateMember();
         return $this->render('member/index.html.twig', ['member' => $member]);
