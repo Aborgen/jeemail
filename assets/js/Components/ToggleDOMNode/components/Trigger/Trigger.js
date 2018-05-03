f5e7db31bfec25b8f982a9809700f769a2430ac8
Alexander Borgen <AlexanderBorgen@gmail.com>
2018-05-02 17:13:31 -0700

    Update DropDown and ExpandCollapse to use ToggleDOMNode, rather than repeating work

--- assets/js/Components/ToggleDOMNode/components/Trigger/Trigger.js
+++ assets/js/Components/ToggleDOMNode/components/Trigger/Trigger.js
@@ -1,13 +1,26 @@
 import React, { PureComponent } from 'react';
+import PropTypes                from 'prop-types';
 
 class Trigger extends PureComponent {
 
     render() {
-        return (
-            <div ref="trigger" {...this.props}></div>
+        const { context, parentName, body } = this.props;
+        const classOrClasses = parentName !== undefined
+            ? `${context}Trigger ${parentName}Trigger`
+            : `${context}Trigger`;
 
+        return (
+            <div className = { classOrClasses }>
+                { body }
+            </div>
         );
     }
 }
 
 export default Trigger;
+
+Trigger.propTypes = {
+    context   : PropTypes.string.isRequired,
+    parentName: PropTypes.string.isRequired,
+    body      : PropTypes.object.isRequired
+}
