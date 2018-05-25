c4594458abf3f47d498b21f21b542454c4806084
Alexander Borgen <AlexanderBorgen@gmail.com>
2018-05-25 16:12:47 -0700

    Update DisplayBlock to use Switch and Route instead of native js switch()

--- assets/js/Components/DisplayBlock/DisplayBlock.js
+++ assets/js/Components/DisplayBlock/DisplayBlock.js
@@ -1,29 +1,80 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes            from 'prop-types';
+import { Route, Switch }    from 'react-router-dom';
 
-// Components
 import EmailBlock    from './components/EmailBlock/EmailBlock';
 import SettingsBlock from './components/SettingsBlock/SettingsBlock';
 import ThemesBlock   from './components/ThemesBlock/ThemesBlock';
-class DisplayBlock extends Component {
+
+class DisplayBlock extends PureComponent {
     render() {
-        const type = this.props.blockType;
-        let showIt;
-        // eslint-disable-next-line
-        switch (type) {
-            case "email":
-                showIt = <EmailBlock />;
-                break;
-            case "settings":
-                showIt = <SettingsBlock />;
-                break;
-            case "themes":
-                showIt = <ThemesBlock />;
-                break;
-        }
+        const { type } = this.props;
         return (
-            showIt
+            <Switch>
+                <Route path = "/email" component    = { EmailBlock } />
+                <Route path = "/settings" component = { SettingsBlock } />
+                <Route path = "/theme" component    = { ThemesBlock } />
+            </Switch>
         );
     }
 }
 
 export default DisplayBlock;
+
+DisplayBlock.propTypes = {
+    member: PropTypes.shape({
+        address   : PropTypes.string.isRequired,
+        birthday  : PropTypes.string.isRequired,
+        email     : PropTypes.string.isRequired,
+        first_name: PropTypes.string.isRequired,
+        last_name : PropTypes.string.isRequired,
+        full_name : PropTypes.string.isRequired,
+        username  : PropTypes.string.isRequired,
+        gender    : PropTypes.string.isRequired,
+        phone     : PropTypes.string.isRequired,
+        icon      : PropTypes.shape({
+            icon_large : PropTypes.string.isRequired,
+            icon_medium: PropTypes.string.isRequired,
+            icon_small : PropTypes.string.isRequired
+        }).isRequired,
+        settings: PropTypes.object.isRequired,
+        theme   : PropTypes.object.isRequired
+    }).isRequired,
+    blocked: PropTypes.arrayOf(PropTypes.shape({
+        id   : PropTypes.number.isRequired,
+        email: PropTypes.string.isRequired
+    })).isRequired,
+    contacts: PropTypes.arrayOf(PropTypes.shape({
+        id          : PropTypes.number.isRequired,
+        name        : PropTypes.string.isRequired,
+        email       : PropTypes.string.isRequired,
+        type        : PropTypes.string,
+        relationship: PropTypes.string,
+        nickname    : PropTypes.string,
+        birthday    : PropTypes.string,
+        phone       : PropTypes.string,
+        job_title   : PropTypes.string,
+        website     : PropTypes.string,
+        notes       : PropTypes.string
+    })).isRequired,
+    organizers: PropTypes.shape({
+        categories: PropTypes.arrayOf(PropTypes.shape({
+            name      : PropTypes.string.isRequired,
+            slug      : PropTypes.string.isRequired,
+            visibility: PropTypes.bool.isRequired
+        }).isRequired).isRequired,
+        labels: PropTypes.shape({
+            default: PropTypes.arrayOf(PropTypes.shape({
+                name      : PropTypes.string.isRequired,
+                slug      : PropTypes.string.isRequired,
+                visibility: PropTypes.bool.isRequired,
+            }).isRequired).isRequired,
+            user: PropTypes.arrayOf(PropTypes.shape({
+                name      : PropTypes.string.isRequired,
+                slug      : PropTypes.string.isRequired,
+                visibility: PropTypes.bool.isRequired,
+            })).isRequired,
+        }).isRequired
+    }).isRequired,
+    emails: PropTypes.objectOf(PropTypes.array).isRequired
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,10 @@ import React                 from 'react';
 import ReactDOM              from 'react-dom';
 import Jeemail               from './Jeemail';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<Jeemail />, document.getElementById('root'));
+ReactDOM.render((
+    <Router>
+        <Jeemail />
+    </Router>), document.getElementById('root'));
 registerServiceWorker();
