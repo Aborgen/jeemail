af6aa44c68b8afdec89b45b77d67bbad7028d1b5
Alexander Borgen <AlexanderBorgen@gmail.com>
2018-06-14 16:59:40 -0700

    Add temporary components for various /settings routes

---
+++ assets/js/Components/DisplayBlock/components/SettingsBlock/components/Blocked/Blocked.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Blocked extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Blocked users are here!</h1>
+            </div>
+        );
+    }
+}
+
+export default Blocked;
---
+++ assets/js/Components/DisplayBlock/components/SettingsBlock/components/Theme/Theme.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Theme extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Set your theme here!</h1>
+            </div>
+        );
+    }
+}
+
+export default Theme;
