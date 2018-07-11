a932617c144c9ab0c635ba963305741c16af8ce7
Alexander Borgen <AlexanderBorgen@gmail.com>
2018-07-11 16:50:53 -0700

    Add EventListener that updates member's last_active field in the Member table

---
+++ src/EventListener/ActivityListener.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\EventListener;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\Security\Core\Security;
+
+use App\Entity\Member;
+
+class ActivityListener
+{
+    private $em;
+    private $security;
+    private $member;
+
+    public function __construct(EntityManagerInterface $em, Security $security)
+    {
+        $this->em       = $em;
+        $this->security = $security;
+    }
+
+    private function getMember()
+    {
+        if(!isset($this->member)) {
+            $this->member = $this->security->getToken()->getUser();
+        }
+
+        return $this->member;
+    }
+
+    public function onKernelController(FilterControllerEvent $event)
+    {
+        // Check that the current request is a "MASTER_REQUEST"
+        // Ignore any sub-request
+        if(!$event->isMasterRequest()) {
+            return;
+        }
+
+        $member = $this->getMember();
+        if($member instanceof Member) {
+            if(!$member->isActiveNow()) {
+                $member->setLastActive(new \DateTime());
+                $this->em->flush($member);
+            }
+        }
+    }
+}
